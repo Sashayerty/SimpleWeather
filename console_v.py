804b6142e00f86137f9198af58f43521d92257d1
Alex <Sashayerty@ya.ru>
2024-06-15 02:10:16 +0300

    Улучшил UX от консольной версии

--- console_v.py
+++ console_v.py
@@ -1,13 +1,11 @@
 from data.get_weather import get_weather as get
 from data.simple_weather_action import simple_weather_action as hi
 
-def simple_weather(place):
-    print(get(place))
+def simple_weather(place)-> None:
+    print(get(place).replace('Follow @igor_chubin for wttr.in updates', ""))
 
-def main():
-    if __name__ == '__main__':
-        hi()
-        simple_weather(input('Введите место:'))
+if __name__ == '__main__':
+    hi()
+    simple_weather(input('Введите место:'))
 
-main()
 
