7ed7049edac8606d310f8b0059cd660213753a67
Alex <Sashayerty@ya.ru>
2024-06-15 17:00:51 +0300

    Добавил log консоли при обновлении/изменении места

--- data/get_weather.py
+++ data/get_weather.py
@@ -1,4 +1,5 @@
 from requests import request
 def get_weather(place:str) -> str:
     res = request(url=f'http://wttr.in/{place}?Tq', method='GET').text
+    print(f'Получены данные для {place}/{place} data received')
     return res
